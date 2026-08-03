9bc58a433fb099584cfc0fc7a6c4e28e6dc190a6
Jonathan Byrd <reckonidid@gmail.com>
2026-08-03 14:03:12 -0400

    Move simulator files to GitHub Pages root

--- supernova.js
+++ supernova.js
@@ -839,9 +839,6 @@ function updateModeControls() {
     mSlider.value = String(m);
     mValue.textContent = String(m);
 
-    updateSliderFill(lmaxSlider);
-    updateSliderFill(mSlider);
-
 }
 
 function updateRemnantFromControls() {
@@ -867,42 +864,6 @@ function finishRemnantUpdate() {
     }
 
     updateRemnantFromControls();
-}
-
-function updateSliderFill(
-    sliderElement
-) {
-    const minimum =
-        Number(sliderElement.min);
-
-    const maximum =
-        Number(sliderElement.max);
-
-    const value =
-        Number(sliderElement.value);
-
-    const range =
-        maximum - minimum;
-
-    const percentage =
-        range > 0
-            ? (
-                (value - minimum)
-                /
-                range
-            ) * 100
-            : 0;
-
-    sliderElement.style.setProperty(
-        "--slider-background",
-        `linear-gradient(
-            to right,
-            var(--slider-color) 0%,
-            var(--slider-color) ${percentage}%,
-            #eeeeee ${percentage}%,
-            #eeeeee 100%
-        )`
-    );
 }
 
 /*
